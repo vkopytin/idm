9bb2cc43770c9a180c9eb2e28e7c89071ab6061b
Volodymyr Kopytin <volodymyr.kopytin@gmail.com>
2024-05-31 21:19:43 +0200

    Updating CORS

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
   options.AddPolicy(name: apiCorsPolicy,
   builder =>
   {
-    builder.WithOrigins("local:*", "localhost:*", "azurewebsites.net:*")
+    builder.WithOrigins("local", "localhost", "azurewebsites.net", "azurewebsites.net:4200")
       .AllowAnyHeader()
       .AllowAnyMethod()
       .AllowCredentials();
